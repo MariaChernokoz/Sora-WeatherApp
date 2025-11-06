98559408c121f9d0676c5f35c59442a6ae1ce3fe
MariaChernokoz <me@chernokoz.ru>
2025-11-06 22:41:55 +0300

    Initial project setup with basic structure

---
+++ Sora/Sora/SoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoraApp.swift
+//  Sora
+//
+//  Created by Chernokoz on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
