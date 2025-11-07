b6026916779bd65e3eb4f13af85c8789e64a376f
MariaChernokoz <me@chernokoz.ru>
2025-11-07 18:47:54 +0300

    TabBar added. VideoBackgroundView implementation.

--- Sora/Sora/ContentView.swift
+++ Sora/Sora/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var temperature = "22°"
+    @State private var weatherDescription = "Идеальный день для маленького приключения"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            VideoBackgroundView(videoName: "totoro_rain_1 2") //"totoro_rain_1"
+            
+            VStack(spacing: 20) {
+                HStack(spacing: 10) {
+                    Text(temperature)
+                        .font(.system(size: 72, weight: .bold))
+                        .foregroundColor(.primary.opacity(0.8))
+                    
+                    Text(weatherDescription)
+                        .font(.system(size: 18, weight: .light))
+                        .foregroundColor(.primary.opacity(0.8))
+                }
+                .padding(.vertical, 30)
+                .padding(.horizontal, 20)
+                .glassEffect(.clear)
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct WeatherDetailView: View {
+    let icon: String
+    let value: String
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(icon)
+                .font(.title2)
+            Text(value)
+                .font(.caption)
+                .foregroundColor(.white.opacity(0.8))
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(.ultraThinMaterial)
+        )
     }
 }
 
---
+++ Sora/Sora/Views/Screens/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Sora
+//
+//  Created by Chernokoz on 07.11.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
