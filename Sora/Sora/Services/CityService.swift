a01ddd3018d6a29d8876e141654c3fa50a8222e9
MariaChernokoz <me@chernokoz.ru>
2025-11-10 02:59:16 +0300

    CityService implementation (1.0).

--- Sora/Sora/Services/CityService.swift
+++ Sora/Sora/Services/CityService.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 import CoreLocation
-import MapKit
+
+final class CityService {
+    
+    private let geocoder = CLGeocoder()
+    
+    func getCoordinates(forCityName cityName: String) async throws-> CLLocationCoordinate2D {
+        
+        let placemarks = try await geocoder.geocodeAddressString(cityName)
+        guard let location = placemarks.first?.location else {
+            throw CityServiceError.cityNotFound
+        }
+        
+        return location.coordinate
+    }
+}
+
+
+enum CityServiceError: Error {
+    case cityNotFound
+    case generalGeocodingError
+}
--- Sora/Sora/Views/Screens/CitiesView.swift
+++ Sora/Sora/Views/Screens/CitiesView.swift
@@ -22,40 +22,7 @@ struct CitiesView: View {
                 TextField("Введите название города...", text: $cityInput)
                     .padding(16)
                     .glassEffect(.clear)
-                
-                Spacer()
-                
-                VStack(spacing: 20) {
-                    Image(systemName: "list.bullet")
-                        .font(.system(size: 60))
-                        .foregroundColor(.white.opacity(0.8))
-                    
-                    Text("Список городов")
-                        .font(.title2)
-                        .foregroundColor(.white)
-                    
-                    Text("Здесь будет список ваших городов")
-                        .font(.subheadline)
-                        .foregroundColor(.white.opacity(0.7))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 40)
-                    
-                    Button(action: {
-                        
-                    }) {
-                        HStack {
-                            Image(systemName: "plus.circle.fill")
-                            Text("Добавить город")
-                        }
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .glassEffect()
-                    }
-                }
-                .padding(40)
-                .glassEffect(.clear)
-                
+
                 Spacer()
             }
             .padding()
